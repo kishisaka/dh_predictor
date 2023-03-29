00a84978df07d42c31485fd62a430ef55da1bf4b
kurt <kurt.ishisaka@gmail.com>
2023-03-28 21:44:23 -0700

    fix shadow name

--- applicant_scorer.py
+++ applicant_scorer.py
@@ -4,15 +4,15 @@ from jsonconstants import JSONConstants
 
 
 class Scorer:
-    def score_people(self, people, predictor: predictor.Predictor, people_name: str):
+    def score_people(self, people, predictor_impl: predictor.Predictor, people_name: str):
         scores = []
         for person in people:
             score = {JSONConstants.name: person[JSONConstants.name], JSONConstants.score: (
-                predictor.predict(Person(person)))}
+                predictor_impl.predict(Person(person)))}
             scores.append(score)
         scored_applicants = {people_name: scores}
         return scored_applicants
 
-    def process_file(self, teams_and_applicants, people_type: str, label: str, predictor: predictor.Predictor):
-        return self.score_people(teams_and_applicants[people_type], predictor, label)
+    def process_file(self, teams_and_applicants, people_type: str, label: str, predictor_impl: predictor.Predictor):
+        return self.score_people(teams_and_applicants[people_type], predictor_impl, label)
 
