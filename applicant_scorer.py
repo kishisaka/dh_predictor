f6fe58d78f8b6190b3eb3d50eff7f9eaa63dddd3
kurt <kurt.ishisaka@gmail.com>
2023-03-28 19:52:06 -0700

    small refactor to bring load dependency outside of the Scorer

--- applicant_scorer.py
+++ applicant_scorer.py
@@ -1,22 +1,18 @@
-import json
-
 import dh_predictor
 from person import Person
 from jsonconstants import JSONConstants
 
 
 class Scorer:
-    def score_people(self, teams_and_applicants, predictor: dh_predictor.Predictor, people_name: str):
+    def score_people(self, people, predictor: dh_predictor.Predictor, people_name: str):
         scores = []
-        for applicant in teams_and_applicants:
-            score = {JSONConstants.name: applicant[JSONConstants.name], JSONConstants.score: (
-                predictor.predict(Person(applicant)))}
+        for person in people:
+            score = {JSONConstants.name: person[JSONConstants.name], JSONConstants.score: (
+                predictor.predict(Person(person)))}
             scores.append(score)
         scored_applicants = {people_name: scores}
         return scored_applicants
 
-    def process_file(self, file: str, people_type: str, label: str, predictor: dh_predictor.Predictor):
-        json_file = open(file)
-        teams_and_applicants = json.load(json_file)
+    def process_file(self, teams_and_applicants, people_type: str, label: str, predictor: dh_predictor.Predictor):
         return self.score_people(teams_and_applicants[people_type], predictor, label)
 
--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from jsonconstants import JSONConstants
 def print_scores():
     scorer = applicant_scorer.Scorer()
     predictor = Predictor()
+    json_file = json.load(open("data.json"))
     print(json.dumps(
-        scorer.process_file("data.json", JSONConstants.applicants, "scoredApplicants", predictor), indent=4)
+        scorer.process_file(json_file, JSONConstants.applicants, "scoredApplicants", predictor), indent=4)
     )
 
 print_scores()
